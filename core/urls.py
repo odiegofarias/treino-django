a8076fd2c1aba5fadc1c0a461dc1c7d5436286fa
odiegofarias <diego.farias.fernandes@gmail.com>
2022-11-29 21:19:40 -0300

    Terminando de treinar CBV's com CRUD completo

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name='core'
 
 urlpatterns = [
-    path('', views.ListaFuncionarios.as_view(), name='lista-funcionarios')
+    path('', views.ListaFuncionarios.as_view(), name='lista-funcionarios'),
+    path('index/', views.IndexTemplateView.as_view(), name='index'),
+    path('funcionario/<id>', views.FuncionarioUpdateView.as_view(), name='atualiza-funcionario'),
+    path('funcionario/exclui/<pk>', views.FuncionarioDeleteView.as_view(), name='exclui-funcionario'),
+    path('funcionario/cadastrar', views.FuncionarioCreateView.as_view(), name='cria-funcionario'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from .models import Funcionario
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView, UpdateView, DeleteView, CreateView
 
 
 def lista_funcionarios(request):
@@ -12,6 +13,7 @@ def lista_funcionarios(request):
 class ListaFuncionarios(ListView):
     template_name = 'core/funcionarios.html'
     model = Funcionario
+    # nome padrão = object
     context_object_name = 'funcionarios'
 
 
@@ -22,3 +24,45 @@ def cria_funcionario(request):
             return redirect('core:lista-funcionarios')
     else:
         return render(request, 'core/form.html', {'form': form})
+
+
+class IndexTemplateView(TemplateView):
+    template_name = 'index.html'
+
+
+class FuncionarioUpdateView(UpdateView):
+    template_name = 'atualiza.html'
+    model = Funcionario
+    fields = '__all__'
+    context_object_name = 'funcionario'
+
+    def get_object(self, queryset=None):
+        funcionario = None
+
+        id = self.kwargs.get(self.pk_url_kwargs)
+        slug = self.kwargs.get(self.slug_url_kwargs)
+
+        if id is not None:
+            funcionario = Funcionario.objects.filter(id=id).first()
+        
+        elif slug is not None:
+            campo_slug = self.get_slug_field()
+
+            funcionario = Funcionario.objects.filter(**{campo_slug: slug}).first()
+
+        return funcionario
+
+
+class FuncionarioDeleteView(DeleteView):
+    template_name = 'core/exclui.html'
+    model = Funcionario
+    context_object_name = 'funcionario'
+    success_url = reverse_lazy('core:lista-funcionarios')
+
+
+class FuncionarioCreateView(CreateView):
+    template_name = 'core/cria.html'
+    model = Funcionario
+    form_class = InseFuncionarioForm
+    success_url = reverse_lazy('core:lista-funcionarios')
+
