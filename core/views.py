e875aa6d7351d7319a1a11fd7f1353b363a9e162
odiegofarias <diego.farias.fernandes@gmail.com>
2022-11-29 21:02:31 -0300

    Treinando criação de views com FBV e CBV

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Funcionario
+from django.views.generic import ListView
 
-# Create your views here.
+
+def lista_funcionarios(request):
+    funcionarios = Funcionario.objetos.all()
+
+    return render(request, 'core/funcionarios.html', {'funcionarios': funcionarios})
+
+
+class ListaFuncionarios(ListView):
+    template_name = 'core/funcionarios.html'
+    model = Funcionario
+    context_object_name = 'funcionarios'
+
+
+def cria_funcionario(request):
+    if request.method == 'POST':
+        form = FormularioDeCriacao(request.POST)
+        if form.is_valid():
+            return redirect('core:lista-funcionarios')
+    else:
+        return render(request, 'core/form.html', {'form': form})
